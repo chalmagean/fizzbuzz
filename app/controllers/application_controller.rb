0de8e8650e8288da3ad84b3b184acc1165badecd
Cezar Halmagean <cezar@mixandgo.com>
2016-05-18 09:11:36 +0300

    Token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate_user_from_token!
   before_action :authenticate_user!
+
+  private
+
+    def authenticate_user_from_token!
+      user_token = params[:auth_token].presence
+      user = user_token && User.find_by_authentication_token(user_token.to_s)
+
+      if user && Devise.secure_compare(user.authentication_token, params[:auth_token])
+        sign_in user, store: false
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   get '/:page', to: 'numbers#index'
   get '/favorite/:number', to: 'numbers#favorite', as: :favorite
   root 'numbers#index'
-  # resources :numbers, only: :index
 end
